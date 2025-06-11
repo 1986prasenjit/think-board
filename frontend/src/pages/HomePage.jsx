e6ca57165fd9eef1c515547611f5efef3c6b258e
1986prasenjit <prasenjitbasu44@gmail.com>
2025-06-12 01:22:48 +0530

    added RateLimiterUI component and Updated Home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
+import RateLimiterUI from '../components/RateLimiterUI';
 
 const HomePage = () => {
+  const [ isRateLimit, setIsRateLimit ] = useState(false);
   return (
     <div className="min-h-screen">
       <Navbar />
+
+      {
+        isRateLimit && <RateLimiterUI />
+      }
     </div>
   )
 }
