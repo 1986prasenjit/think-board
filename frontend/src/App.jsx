d1a5e6780708e8327347f4f8b591aa471262d864
1986prasenjit <prasenjitbasu44@gmail.com>
2025-06-12 00:10:30 +0530

    set up the frontend, added required packages and added HomePage, CreateNotePage, NoteDetailsPage and also added Navbar Component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from 'react-router'
+import './App.css'
+import HomePage from './pages/HomePage'
+import CreateNotePage from './pages/CreateNotePage'
+import NoteDetailsPage from './pages/NoteDetailsPage'
+import toast from 'react-hot-toast'
+
+function App() {
+
+  return (
+    <div data-theme="forest">
+      <Routes>
+        <Route
+          path='/'
+          element={<HomePage />}
+        />
+        <Route
+          path='/create-note'
+          element={<CreateNotePage />}
+        />
+        <Route
+          path='/note/:id'
+          element={<NoteDetailsPage />}
+        />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
